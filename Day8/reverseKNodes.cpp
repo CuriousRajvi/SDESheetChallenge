0e9f2d5bb60db5ede68c9411a97837a54a4cba22
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-06-15 14:44:13 +0530

    Update reverseKNodes.cpp

--- Day8/reverseKNodes.cpp
+++ Day8/reverseKNodes.cpp
@@ -6,23 +6,51 @@
         class Node
         {
         public:
-	        int data;
-	        Node *next;
-	        Node(int data)
-	        {
-		        this->data = data;
-		        this->next = NULL;
-	        }
+            int data;
+            Node *next;
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
         };
 
 *****************************************************************/
 
 Node *getListAfterReverseOperation(Node *head, int n, int b[]){
-	// Write your code here.
-     Node *node;
-    for(int i=0;i<n;i++){
-        cout<<b[i];
+    // Write your code here.
+    if(head==NULL|| head->next==NULL) return head;
+    Node *h=head;
+    int len=0;
+    while(h!=NULL){
+        len++;
+        h=h->next;
     }
-    return node;
-        
+    Node *dummy;
+    dummy->next=head;
+    Node *pre=dummy;
+    Node *cur=dummy;
+    Node *nex=dummy;
+    
+    int j=0;
+    
+    while(len>0 && j<n){
+        cur=pre->next;
+        nex=cur->next;
+        int x=(b[j]<=len)?b[j]:len;
+        if(x==0){
+            j++;
+            continue;
+        }
+        for(int i=1;i<x;i++){
+            cur->next=nex->next;
+            nex->next=pre->next;
+            pre->next=nex;
+            nex=cur->next;
+        }
+        pre=cur;
+        len-=b[j];
+        j++;
+    }
+    return dummy->next;
 }
