fea86a1b8afa8c01aa730b7e7d2793854ec11be3
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-06-15 14:43:26 +0530

    Create reverseKNodes.cpp

---
+++ Day8/reverseKNodes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+	// Write your code here.
+     Node *node;
+    for(int i=0;i<n;i++){
+        cout<<b[i];
+    }
+    return node;
+        
+}
