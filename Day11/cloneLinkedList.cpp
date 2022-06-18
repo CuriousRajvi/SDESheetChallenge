84a3b914856a2ee6bcf7b4a2bc1795fb1fd8d746
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-06-18 17:49:50 +0530

    Create cloneLinkedList.cpp

---
+++ Day11/cloneLinkedList.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the LinkedListNode class structure
+
+    template <typename T>   
+    class LinkedListNode
+    {
+        public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode<T> *random;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*************************************************************/
+
+LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+{
+    // Write your code here.
+    if(!head ) return head;
+   map<LinkedListNode<int> *,LinkedListNode<int> *> mp;
+   
+    LinkedListNode<int> *ptr=head;
+    while(ptr){
+        mp[ptr]=new LinkedListNode<int> (ptr->data);
+        ptr=ptr->next;
+    }
+    ptr=head;
+    while(ptr){
+        mp[ptr]->next=mp[ptr->next];
+        mp[ptr]->random=mp[ptr->random];
+        ptr=ptr->next;
+    }
+    return mp[head];
+}
