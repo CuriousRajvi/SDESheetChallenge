1486888156147a89dc1935d7c993fdd1b6b4b21c
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-07-18 18:52:44 +0530

    Create getTopViewOfBinaryTree.cpp

---
+++ Day37/getTopViewOfBinaryTree.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+vector<int> getTopView(TreeNode<int> *root) {
+   vector<int> ans;
+   if (root==NULL) return ans;
+    queue<pair<TreeNode<int>*,int>> q;
+   q.push({root,0});
+   
+   map<int,int> mp;
+   while(!q.empty()){
+       TreeNode<int> * node = q.front().first;
+       int line = q.front().second;
+        q.pop();
+      if(mp.count(line)==0) mp[line]=node->val;
+       if(node->left){ q.push({node->left,line-1});}
+       if(node->right){ q.push({node->right,line+1});}
+       
+   }
+for(auto it: mp){
+   ans.push_back(it.second);
+}
+return ans;
+}
